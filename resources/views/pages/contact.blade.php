0dbf887e597cab33727c595760005af8a999a7ce
= <=>
2021-03-15 14:42:30 +0700

    tugas lutfi

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Halaman About')
+@section('content')
+    Halaman About
+
+    <x-modal theme="primary">
+        Modal Home
+    </x-modal>
+    @stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Halaman Home')
+@section('content')
+    Halaman Home Selamat datang
+
+    <x-modal theme="danger">
+        Modal Home
+    </x-modal>
+    @stop
